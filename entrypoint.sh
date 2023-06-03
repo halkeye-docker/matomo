c09385b2fd907c866f18dcd534f15f60b0c53867
Gavin Mogan <git@gavinmogan.com>
2023-06-02 23:03:06 -0700

    fix: entrypoint should update /tmp/config not the empty /var/www one

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 envsubst < /config.ini.tmpl > /tmp/config.ini.php
 while IFS=',' read -ra MATOMO_TRUSTED_HOSTS; do
 	for i in "${MATOMO_TRUSTED_HOSTS[@]}"; do
-		perl -pi -e "s{#trusted_hosts}{trusted_hosts[] = \"$i\"\n#trusted_hosts}g" /var/www/html/config/config.ini.php
+		perl -pi -e "s{#trusted_hosts}{trusted_hosts[] = \"$i\"\n#trusted_hosts}g" /tmp/config.ini.php
 	done
 done <<< "$MATOMO_TRUSTED_HOSTS"
 sed -i'' '/#trusted_hosts/d' /tmp/config.ini.php
