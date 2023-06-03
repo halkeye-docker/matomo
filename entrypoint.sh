18b77875cc9ff2b8c6be7aef7f2f0058dd974477
Gavin Mogan <git@gavinmogan.com>
2023-06-02 22:37:23 -0700

    make sure config.ini matches closer to actual so app doesn't need write access

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ while IFS=',' read -ra MATOMO_TRUSTED_HOSTS; do
 		perl -pi -e "s{#trusted_hosts}{trusted_hosts[] = \"$i\"\n#trusted_hosts}g" /var/www/html/config/config.ini.php
 	done
 done <<< "$MATOMO_TRUSTED_HOSTS"
-perl -pi -e '/#trusted_hosts/d' /tmp/config.ini.php
+sed -i'' '/#trusted_hosts/d' /tmp/config.ini.php
 cat /tmp/config.ini.php > /var/www/html/config/config.ini.php
 rm /tmp/config.ini.php
 
